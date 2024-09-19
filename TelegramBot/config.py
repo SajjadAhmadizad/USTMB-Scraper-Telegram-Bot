011554e0606ec0c09052e460b50631a07bcb4547
Sajjad Ahmadizad <96223844+SajjadAhmadizad@users.noreply.github.com>
2024-09-20 03:09:18 +0330

    Allowed Users removed

--- TelegramBot/config.py
+++ TelegramBot/config.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-ALLOWED_USER_IDS = [int(id) for id in os.getenv("ALLOWED_USER_IDS").split(",")]
+# ALLOWED_USER_IDS = [int(id) for id in os.getenv("ALLOWED_USER_IDS").split(",")]
